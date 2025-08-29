6902534f80b072cac26b48ad2a60177777a7281e
Jose Carlos <jose.quinterocedeno@ucr.ac.cr>
2025-08-28 18:42:18 -0600

    feat: agregar modulo y script de carga y limpieza de las bases de datos, issue #7

--- src/data/limpieza_datos.py
+++ src/data/limpieza_datos.py
@@ -1,1 +1,46 @@
 # Funciones para carga y limpieza de datos
+import os
+import sys
+
+sys.path.append(os.path.join(os.getcwd(), "src/data"))
+
+from LimpiadorDatos import LimpiadorDatos
+
+rutas = {
+    "sobrepeso": "data/raw/base_sobrepeso.csv",
+    "obesidad": "data/raw/base_obesidad.csv",
+    "combinada": "data/raw/base_combinada.csv",
+}
+
+ld = LimpiadorDatos(rutas)
+
+ld.cargar_dfs()
+
+df = ld.unir(id_vars=["CODE", "DISTRICT", "PROVINCE", "CANTON"], ordenar_por="CODE")
+
+traducciones = {
+    "CODE": "codigo",
+    "DISTRICT": "distrito",
+    "PROVINCE": "provincia",
+    "CANTON": "canton",
+    "Condition": "condicion",
+    "N": "total",
+    "n": "subtotal",
+    "Prevalence": "prevalencia",
+    "Unemployment": "desempleo",
+    "Urban Population": "poblacion_urbana",
+    "Critical Deprivation": "privacion_critica",
+    "Population under 14": "poblacion_menor_14",
+    "Single mother homes": "hogares_monomarentales",
+    "Occupants per home": "ocupantes_por_hogar",
+    "Schooling years": "anos_escolaridad",
+}
+
+df = df.rename(columns=traducciones)
+
+ld.df = df
+
+# Guardar resultado
+ld.guardar("data/clean/datos_limpios.csv")
+
+print(df.head())
